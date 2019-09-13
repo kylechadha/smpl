aab03b08ca454ef2a88541c7f97304bde19c31b6
Kyle Chadha <kyle.chadha@gmail.com>
2019-09-12 21:12:22 -0700

    Build SVG ticking clock

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
+// To Do's
+// - Add animation on load for text and date
+// - Add ability to change font sizes and colors (perhaps controlled by cursor?), could have icons bottom left
+// -- Scroll through different color themes
+// - Save preferences in local storage
+// - Read up on em's, so you can ensure text always fits inside svg circle
+// - Make everything responsive
+// >> going to have to force an update of the circumference value as well
+
+// ** how did we do functions / JS architecture in the previous extension?
+
+var sweep = $('#sweep');
+var circumference = parseInt(sweep.css('r'), 10) * 2 * Math.PI;
+
 function update() {
-  var now = moment()
+  var now = moment();
   $('#time').html(now.format('h:mm'));
   $('#date').html(now.format('dddd, MMMM DD'));
-}
 
+  sweep.css('stroke-dashoffset', circumference*(1-(now.seconds()/59)))
+}
 update();
 setInterval(update, 1000);
+
+console.log("Smpl: Created with <3 by Kyle Chadha");
